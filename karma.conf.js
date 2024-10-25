402cb5f8c826dd654b8159b3e1f407442fe7e8f1
Seb <sebastian.berchtold@hftm.ch>
2024-10-25 14:05:50 +0200

    karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,14 @@
 module.exports = function (config) {
   config.set({
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadlessCI'],  // Use ChromeHeadless in CI environment
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu']
-      }
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--headless', '--remote-debugging-port=9222'],
+      },
     },
-    singleRun: true,
+    singleRun: true, // Ensures that tests run once and then exit
+    // Other configurations...
   });
 };
