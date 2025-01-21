be7a7b5033cd17716f9d79346dd99b94dced66c9
Seb <sebastian.berchtold@hftm.ch>
2025-01-21 19:26:11 +0100

    fix: SHOW BLOGS AGAIN

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,21 @@ import {BlogListComponent} from './features/components/blog-list/blog-list.compo
 import {BlogDetailsComponent} from './features/components/blog-details/blog-details.component';
 
 export const routes: Routes = [
-  {path: '', component: BlogListComponent},
-  {path: 'blogs/:id', component: BlogDetailsComponent},
-  {path: '**', redirectTo: ''},
+  {
+    path: 'blogs-overview',
+    loadComponent: () =>
+      import('./features/components/blog-list/blog-list.component').then(
+        (m) => m.BlogListComponent
+      ),
+  },
+  {
+    path: 'blogs/:id',
+    loadComponent: () =>
+      import('./features/components/blog-details/blog-details.component').then(
+        (m) => m.BlogDetailsComponent
+      ),
+  },
+  { path: '', redirectTo: '/blogs-overview', pathMatch: 'full' },
+  { path: '**', redirectTo: '' },
 ];
+
