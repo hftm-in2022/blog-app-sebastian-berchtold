375744c1f2552c4a0d739dcb13487e5280c084a5
Seb <sebastian.berchtold@hftm.ch>
2025-01-22 16:27:28 +0100

    fix: unused Mat removed

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -5,56 +5,45 @@ import {MatToolbar} from '@angular/material/toolbar';
 import {MatAnchor, MatButton} from '@angular/material/button';
 import {MatMenu, MatMenuTrigger} from '@angular/material/menu';
 import {MatIcon} from '@angular/material/icon';
-import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
-import {MatListItem, MatNavList} from '@angular/material/list';
-import {RouterOutlet} from '@angular/router';
-import {MatHeaderRowDef} from '@angular/material/table';
 
 @Component({
-    selector: 'app-sidebar',
-    standalone: true,
-    imports: [
-        MatToolbar,
-        MatAnchor,
-        MatButton,
-        MatMenuTrigger,
-        MatMenu,
-        MatIcon,
-        MatSidenavContainer,
-        MatSidenavContent,
-        MatSidenav,
-        MatNavList,
-        MatListItem,
-        RouterOutlet,
-        MatHeaderRowDef
-    ],
-    templateUrl: 'sidebar.component.html',
-    styleUrl: 'sidebar.component.scss'
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    MatToolbar,
+    MatAnchor,
+    MatButton,
+    MatMenuTrigger,
+    MatMenu,
+    MatIcon
+  ],
+  templateUrl: 'sidebar.component.html',
+  styleUrl: 'sidebar.component.scss'
 })
 export class SidebarComponent {
-    authenticated = signal<boolean>(false);
-    userName = signal<string>('');
-    canAddBlogs = signal<boolean>(false);
+  authenticated = signal<boolean>(false);
+  userName = signal<string>('');
+  canAddBlogs = signal<boolean>(false);
 
-    constructor(private oidcSecurityService: OidcSecurityService,
-                private userService: UserService) {
-        this.oidcSecurityService.checkAuth().subscribe((res) => {
-            this.authenticated.set(res.isAuthenticated);
+  constructor(private oidcSecurityService: OidcSecurityService,
+              private userService: UserService) {
+    this.oidcSecurityService.checkAuth().subscribe((res) => {
+      this.authenticated.set(res.isAuthenticated);
 
-            if (res.isAuthenticated) {
-                this.userName.set(res.userData.email);
-                this.oidcSecurityService.getAccessToken().subscribe((accessToken) => {
-                    this.canAddBlogs.set(this.userService.hasRole(accessToken, 'user'));
-                });
-            }
+      if (res.isAuthenticated) {
+        this.userName.set(res.userData.email);
+        this.oidcSecurityService.getAccessToken().subscribe((accessToken) => {
+          this.canAddBlogs.set(this.userService.hasRole(accessToken, 'user'));
         });
-    }
+      }
+    });
+  }
 
-    login() {
-        this.oidcSecurityService.authorize();
-    }
+  login() {
+    this.oidcSecurityService.authorize();
+  }
 
-    logout() {
-        this.oidcSecurityService.logoff().subscribe();
-    }
+  logout() {
+    this.oidcSecurityService.logoff().subscribe();
+  }
 }
