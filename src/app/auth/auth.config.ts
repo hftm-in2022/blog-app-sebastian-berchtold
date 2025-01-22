3c2822af41e924e4abac91e5ecefa894254fa611
u236205 <sebastian.berchtold2@sbb.ch>
2025-01-22 11:27:58 +0100

    fix: authentification work completly

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -1,6 +1,6 @@
 import {LogLevel, PassedInitialConfig} from 'angular-auth-oidc-client';
 import {environment} from '../enviroments/environment';
-
+/*
 const redirectUrl = environment.production
   ? `${environment.appUrl}/blog-overview`
   : `${window.location.origin}/blog-overview`;
@@ -9,20 +9,20 @@ const postLogoutRedirectUri = environment.production
   ? `${environment.appUrl}/blog-overview`
   : `${window.location.origin}/blog-overview`;
 
-const silentRenewUrl = environment.production
+const silentRenewUrl = environment
   ? `${environment.appUrl}/silent-renew.html`
   : `${window.location.origin}/silent-renew.html`;
-
+*/
 export const authConfig: PassedInitialConfig = {
   config: {
     authority: `${environment.authUrl}/realms/blog`,
-    redirectUrl: redirectUrl,
-    postLogoutRedirectUri: postLogoutRedirectUri,
+    redirectUrl: window.location.origin,
+    postLogoutRedirectUri: window.location.origin,
     clientId: 'spa-blog',
     scope: 'openid profile email offline_access blogs', // 'openid profile offline_access ' + your scopes
     responseType: 'code',
     silentRenew: true,
-    silentRenewUrl: silentRenewUrl,
+    silentRenewUrl: window.location.origin + '/silent-renew.html',
     renewTimeBeforeTokenExpiresInSeconds: 30,
     logLevel: LogLevel.Debug,
     secureRoutes: [environment.backendUrl],
--- src/app/enviroments/environment.ts
+++ src/app/enviroments/environment.ts
@@ -4,5 +4,4 @@ export const environment = {
     'https://d-cap-blog-backend.whitepond-b96fee4b.westeurope.azurecontainerapps.io',
   authUrl:
     'https://d-cap-keyclaok.kindbay-711f60b2.westeurope.azurecontainerapps.io',
-  appUrl: 'https://witty-island-070f07d10-6.centralus.5.azurestaticapps.net',
 };
